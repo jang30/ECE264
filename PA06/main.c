adf5e051565198c8041051957210321efb065876
jang30 <jang30@purdue.edu>
2014-02-27 15:10:26 -0500

    hw6 finished

---
+++ PA06/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "answer06.h"
+
+int main(int argc, char **argv)
+{
+  partitionOddAndEven(1);
+  partitionOddAndEven(2);
+  partitionOddAndEven(3);
+  partitionOddAndEven(4);
+  partitionOddAndEven(5);
+  partitionOddAndEven(6);
+  partitionOddAndEven(7);
+  
+  
+  return EXIT_SUCCESS;
+}
